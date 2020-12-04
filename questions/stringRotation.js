2906c34590080f2b7c9b3c594d3582640848f835
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-04 14:34:52 +0530

    Added solution for stringRotation.js

--- questions/stringRotation.js
+++ questions/stringRotation.js
@@ -4,6 +4,28 @@ const rotateString = (str, n, direction) => {
   //
   // input str = random ,1, right
   // output mrando
+  let result = "";
+  try {
+    if (str == null || n == null || direction == null) {
+      throw new Error("Invalid Input - No Input");
+    } else if (direction !== "right" && direction != "left") {
+      throw new Error("Invalid Input - Invalid Direction");
+    } else if (direction == "right") {
+      for (i = n; i > 0; i--) {
+        result = result + str[str.length - i];
+      }
+      result = result + str.substring(0, str.length - n);
+      return result;
+    } else if (direction == "left") {
+      result = result + str.substring(n, str.length);
+      for (i = 0; i < n; i++) {
+        result = result + str[i];
+      }
+      return result;
+    }
+  } catch (err) {
+    throw err;
+  }
 };
 
 module.exports = rotateString;
